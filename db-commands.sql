19ad4e0138d5b8585f200763ddae50ad210fddd6
finner <finnseamus@gmail.com>
2015-02-09 22:21:50 +0100

    sql commands

---
+++ db-commands.sql
@@ -0,0 +1,12 @@
+insert into products values (1,5010255079763, "Paperclips Large", "Large Plain Pack of 1000");
+
+select * from products;
+
+delete from products;
+
+
+drop table products;
+
+drop table warehouses;
+
+drop table stock_items;
