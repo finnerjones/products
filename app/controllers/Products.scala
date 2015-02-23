d0e343b37ed21830ec0d65ae0db75d11bac11840
finner <finnseamus@gmail.com>
2015-02-23 22:45:14 +0100

    runtime error when adding a new product - NoSuchElementFound from Product.findByEan

--- app/controllers/Products.scala
+++ app/controllers/Products.scala
@@ -15,6 +15,7 @@ object Products extends Controller {
   private val productForm : Form[Product] = Form(
     mapping(
       "ean" -> longNumber.verifying("validation.ean.duplicate", Product.findByEan(_).isEmpty),
+      //"ean" -> longNumber,
       "name" -> nonEmptyText,
       "description" -> nonEmptyText
     )(Product.apply)(Product.unapply)
@@ -33,11 +34,12 @@ object Products extends Controller {
     Product.findByEan(ean).map {
       product =>
         Ok(views.html.products.details(product))
-    }.getOrElse(NotFound)
+    }.head
 
   }
 
   def newProduct = Action { implicit request =>
+    println("newProduct **************************************+")
     val form = if (flash.get("error").isDefined)
       productForm.bind(flash.data)
     else
@@ -49,15 +51,18 @@ object Products extends Controller {
 
   def save = Action { implicit request =>
     val newProductForm = productForm.bindFromRequest()
+    println("Saving a prodcut *********************")
     newProductForm.fold(
-      hasErrors = { form =>
+      hasErrors = { form => println("An error occurredd ******************************");
         Redirect(routes.Products.newProduct()).
           flashing(Flash(form.data) + ("error" -> Messages("validation.errors")))
       },
       success = { newProduct =>
-          Product.add(newProduct)
+          println("Inserting product *************************");
+          Product.insert(newProduct)
           val message = Messages("products.new.success", newProduct.name)
-          Redirect(routes.Products.show(newProduct.ean)).flashing("success" -> message)
+          //Redirect(routes.Products.show(newProduct.ean)).flashing("success" -> message)
+          Redirect(routes.Products.list()).flashing("success" -> message)
         }
     )
   }
--- app/models/Product.scala
+++ app/models/Product.scala
@@ -1,7 +1,6 @@
 package models
 
-import anorm.SQL
-import anorm.SqlQuery
+import anorm.{Row, SQL, SqlQuery}
 import play.api.db.DB
 import play.api.Play.current
 
@@ -22,7 +21,7 @@ object Product {
 
   def findAll = products.toList.sortBy(_.ean)
 
-  def findByEan(ean : Long) = products.find(_.ean == ean)
+  def findEan(ean : Long) = products.find(_.ean == ean)
 
   def add(product: Product) {
     products = products + product
@@ -37,6 +36,45 @@ object Product {
         row[String]("description")
       )).toList
   }
+
+  def insert(product: Product): Boolean =
+    DB.withConnection {implicit connection =>
+      println("insert prodct ********************");
+      val addedRows = SQL("""insert into products values ({id},{ean},{name},{description})""")
+        .on(
+          "id" -> 2,
+          "ean" -> product.ean,
+          "name" -> product.name,
+          "description" -> product.description).executeUpdate()
+      println("inserted product ************************************")
+      addedRows == 1
+    }
+
+  def findByEanDeprecated(eanToFind: Long): List[Product] = DB.withConnection {
+    implicit connection =>
+      println("findByEan *************************************************")
+      val findEanSQL = SQL("""select * from products where ean = {eanToFind}""")
+      val products:List[Product] = findEanSQL().map( row =>
+        Product(
+          row[Long]("ean"),
+          row[String]("name"),
+          row[String]("description")
+        )).toList
+      products
+  }
+
+
+  def findByEan(productEan: Long): List[Product] = DB.withConnection {
+    implicit connection =>
+      println("findByEan using collect *************************************************")
+      val findProductSQL = SQL("""select * from products where ean = {eanToFind}""")
+      val products:List[Product] = findProductSQL().collect {
+        case Row(Some(ean:Long), Some(name:String),Some(description:String)) =>
+          Product(ean,name,description)
+      }.toList
+      products
+  }
+
 
 
 }
